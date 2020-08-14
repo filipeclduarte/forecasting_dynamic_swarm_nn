57ed77038d4d6a80084e7aed5f2d8c447bcffcae
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2020-08-14 00:50:17 -0300

    ajustando para computar mse para iteracoes

--- funcoes.py
+++ funcoes.py
@@ -91,113 +91,82 @@ def cenarios_dinamicos(serie, window_size, step_size):
         
     return cenarios
 
-# Criando cenários
-def cenarios_execucoes(X, y, w, s, f, modelo, qtd_execucoes = 30):
+# # Criando cenários
+# def cenarios_execucoes(X, y, w, s, f, modelo, qtd_execucoes = 30):
     
-    # gerando os cenários dinâmicos
-    X_I = cenarios_dinamicos(X, w, s)
-    y_I = cenarios_dinamicos(y, w, s)
+#     # gerando os cenários dinâmicos
+#     X_I = cenarios_dinamicos(X, w, s)
+#     y_I = cenarios_dinamicos(y, w, s)
  
-    # calculando a quantidade de iterações
-    T = int(f/s*(len(y)-w)+f)
+#     # calculando a quantidade de iterações
+#     T = int(f/s*(len(y)-w)+f)
     
-    mse_treino = np.zeros((qtd_execucoes, len(y_I) * f))
-    mse_teste = np.zeros((qtd_execucoes, len(y_I) * f))
+#     neuronios = np.arange(2, 26)
+    
+#     mse_treino = np.zeros((qtd_execucoes, len(neuronios),len(y_I) * f))
+#     mse_val = np.zeros((qtd_execucoes, len(neuronios), len(y_I) * f))
+#     mse_teste = np.zeros((qtd_execucoes, len(neuronios),len(y_I) * f))
 
-    execucoes = np.arange(qtd_execucoes)
+#     execucoes = np.arange(qtd_execucoes)
 
-    for execucao in execucoes:
-        # inicializando array para 
-        mse_treino_lista = np.array([])
-        mse_teste_lista = np.array([])
-    
-        print('Execução: ', execucao)
-    
-    # Janelamento
-        for i in np.arange(len(y_I)):
-            print('Janela: ', i)
-            ## Divisão em treinamento e teste
-            X_treino, y_treino, X_teste, y_teste, X_validacao, y_validacao = divisao_dados_temporais(X_I[i], y_I[i], perc_treino=.56, perc_val = .24)
-    
-            ### Treinar rede neural com backprop
-            # setando parâmetros para comparação
-            best_model = 0
-            best_mse = np.inf
+#     for execucao in execucoes:
+#         print('Execução: ', execucao)
 
-            # quantidade de neurônios de 2 até 25
-            neuronios = np.arange(2, 26)
-    
-            # grid search 
-            for j in neuronios:
-                #print('Neurônios: ', j)
-                
-                # treinar NN para f iterações
-                parameters, mse_treino_lista_temp, mse_val_lista_temp, mse_teste_lista_temp = modelo(X_treino.T, y_treino.T, X_validacao.T, y_validacao.T, 
-                                                                                                     X_teste.T, y_teste.T, n_h = j, num_iteracoes = f)
-                
-                # predição na validação
-                y_pred_val = predict2(parameters, X_validacao.T)
-                mse_validacao = compute_cost2(y_pred_val, y_validacao.T, parameters)
-        
-                if mse_validacao < best_mse:
-                    best_model = parameters 
-                    best_mse = mse_validacao
-                    #print('Melhor MSE: ', best_mse)
-                    qtd_neuronios = j
-                        
-                    # salvando as listas com os mse de treino e teste
-                    best_mse_treino = np.array(mse_treino_lista_temp)
-                    best_mse_teste = np.array(mse_teste_lista_temp)
-
-            ## ao final da grid, concatenar as listas de treino e teste    
-            mse_treino_lista = np.concatenate((mse_treino_lista, best_mse_treino), axis = None)
-            mse_teste_lista = np.concatenate((mse_teste_lista, best_mse_teste), axis = None)
+#         # Neuronios
+#         for j,z in zip(neuronios, np.arange(len(neuronios))):
             
-        # salvar lista com os mse de treino para todas as iterações
-        mse_treino[execucao,:] = mse_treino_lista
-        # salvar lista com os mse de teste para todas as iterações
-        mse_teste[execucao,:] = mse_teste_lista
+#             parameters, mse_treino_lista_temp, mse_val_lista_temp, mse_teste_lista_temp = modelo(X.T, y.T, n_h = j, num_iteracoes = f, perc_treino, perc_val)
 
-    return mse_treino, mse_teste
+#             # salvar lista com os mse de treino para todas as iterações
+#             mse_treino[execucao, z,:] = np.array(mse_treino_lista_temp)
+#             # salvar lista com os mse de validacao para todas as iteracoes
+#             mse_val[execucao, z,:] = np.array(mse_val_lista_temp)
+#             # salvar lista com os mse de teste para todas as iterações
+#             mse_teste[execucao, z,:] = np.array(mse_teste_lista_temp)
 
-## Criando avaliação dos resultados
-def avaliacao_resultados(mse_treino, mse_teste):
+#     return mse_treino, mse_val, mse_teste
+
+
+
+
+# ## Criando avaliação dos resultados
+# def avaliacao_resultados(mse_treino, mse_teste):
     
-    # quantidade de janelas
-    ### Garantindo que a quantidade de janelas é igual
-    assert mse_treino.shape[1] == mse_teste.shape[1]
+#     # quantidade de janelas
+#     ### Garantindo que a quantidade de janelas é igual
+#     assert mse_treino.shape[1] == mse_teste.shape[1]
     
-    qtd_iteracoes = mse_treino.shape[1]
+#     qtd_iteracoes = mse_treino.shape[1]
     
-    # Calculando CMF
-    te = mse_treino.sum(axis=1)/qtd_iteracoes
-    ge = mse_teste.sum(axis=1)/qtd_iteracoes
+#     # Calculando CMF
+#     te = mse_treino.sum(axis=1)/qtd_iteracoes
+#     ge = mse_teste.sum(axis=1)/qtd_iteracoes
 
-    # calcular a métrica fator de generalização
-    gf = ge/te
+#     # calcular a métrica fator de generalização
+#     gf = ge/te
 
-    # Média e desvio padrão
-    te_medio = te.mean()
-    te_std = te.std()
+#     # Média e desvio padrão
+#     te_medio = te.mean()
+#     te_std = te.std()
 
-    ge_medio = ge.mean()
-    ge_std = ge.std()
+#     ge_medio = ge.mean()
+#     ge_std = ge.std()
 
-    gf_medio = gf.mean()
-    gf_std = gf.std()
+#     gf_medio = gf.mean()
+#     gf_std = gf.std()
 
-    print('TE medio: ', te_medio)
-    print('TE desvio: ', te_std)
-    print('GE medio: ', ge_medio)
-    print('GE desvio: ', ge_std)
-    print('GF medio: ', gf_medio)
-    print('GF desvio: ', gf_std)
+#     print('TE medio: ', te_medio)
+#     print('TE desvio: ', te_std)
+#     print('GE medio: ', ge_medio)
+#     print('GE desvio: ', ge_std)
+#     print('GF medio: ', gf_medio)
+#     print('GF desvio: ', gf_std)
     
-    resultados = {'TE medio': te_medio,
-    'TE desvio': te_std,
-    'GE medio': ge_medio,
-    'GE desvio':ge_std,
-    'GF medio':gf_medio,
-    'GF desvio':gf_std}
+#     resultados = {'TE medio': te_medio,
+#     'TE desvio': te_std,
+#     'GE medio': ge_medio,
+#     'GE desvio':ge_std,
+#     'GF medio':gf_medio,
+#     'GF desvio':gf_std}
     
-    return resultados
+#     return resultados
